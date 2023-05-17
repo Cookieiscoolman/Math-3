d94000fc80a9ba5618de6b01896ec392dc7c2199
Cookieiscoolman <104867083+Cookieiscoolman@users.noreply.github.com>
2023-05-17 16:19:24 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,19 @@
+
+function addUser(){
+localStorage ="player1_name_input"
+localStorage ="player2_name_input"
+  window.location = "game_login.html"
+}
+
+function send() {
+  number1 = document.getElementById("number1").value;
+  number2 = document.getElementById("number2").value;
+  actual_answer = parseInt(number1) * parseInt(number2);
+}
+question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+input_box = "<br>Anwer : <input type='text' id='input_check_box'>";
+check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+row = question_number + input_box + check_button ;
+document.getElementById("output").innerHTML = row;
+document.getElementById("number1").innerHTML = "";
+document.getElementById("number2").innerHTML = "";
